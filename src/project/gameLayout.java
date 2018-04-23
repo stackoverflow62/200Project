e7f1808080dd7200670e35c291ce6ca2ee5db60f
JEVEGA <jevega@vplincsci047.ad.csbsju.edu>
2018-04-23 15:32:17 -0500

    third commit

---
+++ src/project/gameLayout.java
@@ -0,0 +1,10 @@
+package layout;
+import java.util.*;
+public class gameLayout {
+	//associates a given location label with a set of connected locations
+	private Map<String, Set<String>> connections;
+	//associates a given location with its description object
+	private Map<String, LocationDescription> descriptions;
+	
+	
+}
